8fd977e89793e47ff5b0ecca1fc6ad58cae75e35
Meellibiangaha <80907580+Meellibiangaha@users.noreply.github.com>
2022-12-14 22:09:43 +0300

    initial commit

---
+++ js/slider.js
@@ -0,0 +1,132 @@
+//Карусель
+
+
+
+let carousel = document.querySelector('.carusel');
+let caruselItem = document.querySelectorAll('.carusel_item_li').length - 3;
+console.log(caruselItem); //skolko kartinok vsego
+    let width = 450; //size kartinki
+    let count = 1;  //na skolko img prokrutka
+    let list = carousel.querySelector('.carusel_item_ul');
+    let listElems = carousel.querySelectorAll('.carusel_item_li');
+    let position = 0;
+
+    carousel.querySelector('.item_prev').onclick = function() {
+      if(position == 0){
+        position = -width * caruselItem;
+        list.style.marginLeft = position + 'px';
+        console.log(position);
+      }
+      else{
+        position += width * count;
+        position = Math.min(position, 0);
+        list.style.marginLeft = position + 'px';
+        console.log(position);
+      }
+      
+    };
+
+    carousel.querySelector('.item_next').onclick = function() {
+      if(position == -(width * caruselItem)){
+        position = 0;
+        list.style.marginLeft = position + 'px';
+        console.log(position);
+      }
+      else{
+        position -= width * count;
+        position = Math.max(position, -width * (listElems.length - count));
+        list.style.marginLeft = position + 'px';
+        console.log(position);
+      }
+      
+    };
+
+
+    //slider2
+
+    let slider_2_next = document.querySelector('.sldider_2_next');
+    let slider_2_imgs = document.querySelectorAll('.slider_2_imgs');
+    let slider_2_texts = document.querySelectorAll('.slider_2_text');
+    let j = 0; //current_img
+    slider_2_next.addEventListener('click', function() {
+      ++j;
+      if(j >= slider_2_imgs.length){
+        slider_2_imgs[j-1].classList.add('slide_img_diplay');
+        slider_2_texts[j-1].classList.add('block_3_sliderText_diplay');
+        j = 0;
+        slider_2_imgs[j].classList.remove('slide_img_diplay');
+        slider_2_texts[j].classList.remove('block_3_sliderText_diplay');
+      }
+
+      else{
+        slider_2_imgs[j-1].classList.add('slide_img_diplay');
+        slider_2_texts[j-1].classList.add('block_3_sliderText_diplay');
+        slider_2_imgs[j].classList.remove('slide_img_diplay');
+        slider_2_texts[j].classList.remove('block_3_sliderText_diplay');
+      }
+
+    });
+
+    let slider_2_prev = document.querySelector('.sldider_2_prev');
+    slider_2_prev.addEventListener('click', function() {
+      --j;
+      if(j < 0){
+        slider_2_imgs[j+1].classList.add('slide_img_diplay');
+        slider_2_texts[j+1].classList.add('block_3_sliderText_diplay');
+        j = slider_2_imgs.length - 1;
+        slider_2_imgs[j].classList.remove('slide_img_diplay');
+        slider_2_texts[j].classList.remove('block_3_sliderText_diplay');
+      }
+
+      else{
+        slider_2_imgs[j+1].classList.add('slide_img_diplay');
+        slider_2_texts[j+1].classList.add('block_3_sliderText_diplay');
+        slider_2_imgs[j].classList.remove('slide_img_diplay');
+        slider_2_texts[j].classList.remove('block_3_sliderText_diplay');
+      }
+    });
+
+
+//slider_3
+
+     let slider_3_next = document.querySelector('.sldider_3_next');
+    let slider_3_imgs = document.querySelectorAll('.slider_3_imgs');
+    let slider_3_texts = document.querySelectorAll('.slider_3_text');
+    console.log(slider_3_imgs[1]);
+    let k = 0; //current_img
+    slider_3_next.addEventListener('click', function() {
+      ++k;
+      if(k >= slider_3_imgs.length){
+        slider_3_imgs[k-1].classList.add('slide_img_diplay');
+        slider_3_texts[k-1].classList.add('block_3_sliderText_diplay');
+        k = 0;
+        slider_3_imgs[k].classList.remove('slide_img_diplay');
+        slider_3_texts[k].classList.remove('block_3_sliderText_diplay');
+      }
+
+      else{
+        slider_3_imgs[k-1].classList.add('slide_img_diplay');
+        slider_3_texts[k-1].classList.add('block_3_sliderText_diplay');
+        slider_3_imgs[k].classList.remove('slide_img_diplay');
+        slider_3_texts[k].classList.remove('block_3_sliderText_diplay');
+      }
+    });
+
+    let slider_3_prev = document.querySelector('.sldider_3_prev');
+    slider_3_prev.addEventListener('click', function() {
+      --k;
+      if(k < 0){
+        slider_3_imgs[k+1].classList.add('slide_img_diplay');
+        slider_3_texts[k+1].classList.add('block_3_sliderText_diplay');
+        k = slider_3_imgs.length - 1;
+        slider_3_imgs[k].classList.remove('slide_img_diplay');
+        slider_3_texts[k].classList.remove('block_3_sliderText_diplay');
+      }
+
+      else{
+        slider_3_imgs[k+1].classList.add('slide_img_diplay');
+        slider_3_texts[k+1].classList.add('block_3_sliderText_diplay');
+        slider_3_imgs[k].classList.remove('slide_img_diplay');
+        slider_3_texts[k].classList.remove('block_3_sliderText_diplay');
+      }
+    });
---
+++ js/yakorya.js
@@ -0,0 +1,84 @@
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+let fiolTheme = "styles/color_fiol.css";
+let pinkTheme = "styles/color_pink.css";
+let blueTheme = "styles/color_blue.css";
+let redTheme = "styles/color_red.css";
+let userTheme;
+let theme_arr = [fiolTheme, pinkTheme, blueTheme, redTheme];
+let theme_list = ["color_fiol", "color_pink", "color_blue", "color_red"];
+
+let buttonSwaps = document.querySelectorAll('.swap_theme');
+
+for(let i = 0; i < buttonSwaps.length; i++){
+  buttonSwaps[i].addEventListener('click', function(){
+    var link = document.getElementById("theme_link");
+    
+    var currTheme = link.getAttribute("href");
+    var theme = "";
+    if(currTheme != theme_arr[i])
+    {
+     currTheme = theme_arr[i];
+     theme = theme_list[i];
+     userTheme = currTheme;
+    }
+    if(!userTheme){
+      userTheme = fiolTheme;
+    }
+    else{
+      localStorage.setItem('theme', userTheme);
+      link.setAttribute("href", currTheme);
+    }
+    
+  });
+}
+
+window.onload = function() {
+    let current_theme = localStorage.getItem('theme');
+    document.body.style.display = "none";
+    setTimeout(() => document.body.style.display = "", 1); //я не знаю как, но это убирает ненужную анимацию при f5
+    document.getElementById("theme_link").setAttribute("href", current_theme);
+}
+
+// Modal okno
+
+const closeModal = () => { 
+  const modals = document.querySelectorAll('.modal')
+  if (!modals) {return;}
+  modals.forEach(el => {
+    el.addEventListener('click', e => {
+
+      if (!e.target.closest('.modal__body')) { 
+        el.classList.remove('modal_active');
+      }
+    });
+  });
+}
+closeModal();
+
+
+let podrobneeBtn = document.querySelectorAll('.block_4_button');
+let podrobneeModal = document.querySelectorAll('.block_4_Modal');
+for(let i = 0; i < podrobneeBtn.length; i++){
+  console.log(i);
+  podrobneeBtn[i].addEventListener('click', function() {
+    let modal = podrobneeModal[i];
+  if (!modal) {
+    return;
+  }
+  modal.classList.add('modal_active');
+});
+}
